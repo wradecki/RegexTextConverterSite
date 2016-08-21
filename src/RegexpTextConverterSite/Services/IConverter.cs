9df3b361176516f0f32073df0391bc6d2883e005
Wiktor Radecki <wiktor.radecki@mail.com>
2016-08-21 15:46:37 +0200

    Add: converter interface

---
+++ src/RegexpTextConverterSite/Services/IConverter.cs
@@ -0,0 +1,15 @@
+﻿namespace RegexpTextConverterSite.Services
+{
+    public interface IConverter
+    {
+        /// <summary>
+        /// Convert input using replacement
+        /// 
+        /// </summary>
+        /// <param name="input">input text</param>
+        /// <param name="replacement">when found to replace with replacement</param>
+        /// <param name="options">other conviguration options</param>
+        /// <returns>converted text</returns>
+        string Convert(string input, string replacement = "", object options = null);
+    }
+}
