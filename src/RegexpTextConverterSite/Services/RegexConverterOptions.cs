b64abe09a76fabde5c175211a46db5167d5372a5
Wiktor Radecki <wiktor.radecki@mail.com>
2016-08-21 18:29:48 +0200

    Change Converter Api

---
+++ src/RegexpTextConverterSite/Services/RegexConverterOptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexpTextConverterSite.Services
+{
+    public class RegexConverterOptions : ConverterOptions
+    {
+        public RegexConverterOptions(string pattern, RegexOptions options)
+        {
+            this[nameof(Pattern)] = pattern;
+            this[nameof(RegexOptions)] = options;
+        }
+
+        public string Pattern => (string) this[nameof(Pattern)];
+
+        public RegexOptions RegexOptions => (RegexOptions) this[nameof(RegexOptions)];
+    }
+}
